c10a1d88ffadcaee631836228aca79718a6c409a
Andreas Mischke <andreasmischke@gmail.com>
2014-02-10 23:57:26 +0100

    created index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$type = isset($_GET['type']) ? $_GET['type'] : "crypt";
+if($type = "crypt")
+{
+	$length = isset($_GET['length']) ? (int) $_GET['length'] : 32;
+}
+elseif($type == "memo")
+{
+	$length = isset($_GET['length']) ? (int) $_GET['length'] : 4;
+}
+else
+	die("Unknown type");
+
+
+
+header("Content-Type: text/plain");
+
+echo $length . PHP_EOL;
+echo $type . PHP_EOL;
