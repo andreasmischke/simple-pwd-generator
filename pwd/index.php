148faea5b7bb1b89775c00850610205d2070c5dd
Andreas Mischke <andreasmischke@gmail.com>
2014-02-11 00:25:16 +0100

    First version. Can produce n Strings of length l and type crypt (a-zA-Z0-9)

--- pwd/index.php
+++ pwd/index.php
@@ -2,32 +2,33 @@
 
 header("Content-Type: text/plain");
 
-$type = isset($_GET['type']) ? $_GET['type'] : "crypt";
-$times = isset($_GET['times']) ? (int) $_GET['times'] : 1;
+$type = isset($_GET['type']) && $_GET['type'] != "" ? $_GET['type'] : "crypt";
+$times = isset($_GET['times']) && $_GET['times'] != "" ? (int) $_GET['times'] : 1;
 
 if($type == "crypt")
 {
-	$length = isset($_GET['length']) ? (int) $_GET['length'] : 32;
+	$length = isset($_GET['length']) && $_GET['length'] != "" ? (int) $_GET['length'] : 32;
+	$chars = str_split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+	$passphrases = array();
 	
-	$out = array($length);
-	
-	for(var $i = 0; $i < $length; $i++)
-		array_push($out, "x");
-	
-	$passphrase = join($out);
-	
+	for($k = 0; $k < $times; $k++)
+	{
+		$out = array();
+		for($i = 0; $i < $length; $i++)
+			array_push($out, $chars[rand(0,count($chars)-1)]);
+		
+		array_push($passphrases, join($out));
+	}
 }
-elseif($type == "memo")
-{
-	$length = isset($_GET['length']) ? (int) $_GET['length'] : 4;
-	$passphrase = "";
-}
+#elseif($type == "memo")
+#{
+#	$length = isset($_GET['length']) && $_GET['length'] != "" ? (int) $_GET['length'] : 4;
+#	$passphrase = "";
+#}
 else
 	die("Unknown type");
 
-var_dump($length);
-echo PHP_EOL;
-var_dump($type);
-echo PHP_EOL;
-var_dump($passphrase);
-echo PHP_EOL;
+foreach ($passphrases as $passphrase) {
+	echo $passphrase;
+	echo PHP_EOL;
+}
